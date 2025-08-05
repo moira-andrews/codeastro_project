c85bd9f9583ddc728b96a4982d5a8d3abd1e0a7c
Moira Andrews <moira.lee.andrews@gmail.com>
2025-08-05 15:54:05 -0500

    added python function to fit and plot lightcurve

---
+++ fit_and_plot.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/moira-andrews/codeastro_project/refs/heads/main/bolometric_11fe.txt", header=0, sep='\s+')
+df = df[['Phase', 'L']]
+
+def fitting_function(time,L,order):
+    coeffs = np.polyfit(time,L,order)
+    p = np.poly1d(coeffs)
+    fit_data = p(time)
+    return fit_data
+
+
+app = Dash()
+
+fig = go.Figure()
+fit_data = fitting_function(df['Phase'],df['L'],10)
+
+fig.add_trace(go.Scatter(x=df['Phase'], y=df['L'], mode='markers'))
+fig.add_trace(go.Scatter(x=df['Phase'], y=fit_data, mode='lines'))
+
+app.layout = html.Div(children=[
+    html.H1(children='Lightcurve Fitting'),
+
+    dcc.Slider(
+        id='variable-slider',
+        min=0,
+        max=20,
+        step=1,
+        value=3,
+    ),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+@app.callback(
+    Output('example-graph', 'figure'),
+    Input('variable-slider', 'value')
+)
+
+def update_graph(order,df):
+    fit_data = fitting_function(df['Phase'],df['L'],order)
+
+    fig.add_trace(go.Scatter(x=df['Phase'], y=fit_data, mode='lines')) #updating the fitted trace
+
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
